09a47a4d65e90ca1a72454784cca7c3511391a4e
litingyi <leemarceline015@gmail.com>
2022-11-07 17:36:28 +0800

    002

---
+++ hw6/backend/src/routes/ScoreCard.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import ScoreCard from "./models/ScoreCard.js";
+  const router = Router();
+  router.delete("/cards", ...);
+  router.post("/card", ...);
+  router.get("/cards", ...);
+  export default router;
+ 
